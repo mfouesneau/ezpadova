ea7adadd635ec247ca68ed64deb857684432777d
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2025-04-02 09:52:21 +0200

    Update __init__.py

--- src/ezpadova/__init__.py
+++ src/ezpadova/__init__.py
@@ -3,7 +3,7 @@ from .parsec import get_isochrones, resample_evolution_label
 from . import parsec
 from .interpolate import QuickInterpolator
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 __all__ = ["get_isochrones", "get_Z_isochrones", "get_one_isochrone",
            "get_t_isochrones", "parsec", "QuickInterpolator", "resample_evolution_label"]
